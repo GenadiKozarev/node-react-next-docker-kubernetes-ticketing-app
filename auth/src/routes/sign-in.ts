c6eb449b4768ad235f74251b3a19f155b91fa95b
Genadi <genadi.kozarev@gmail.com>
2023-08-15 12:52:19 +0100

    add an error handling middleware… create consistent error structure that will be sent to the browser

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,29 +1,9 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import express from 'express';
 
 const router = express.Router();
 
-router.post(
-    '/api/users/signin',
-    [
-        body('email').isEmail().withMessage('Email must be valid'),
-        body('password')
-            .trim()
-            .isLength({ min: 4, max: 30 })
-            .withMessage('Password must be between 4 and 30 characters'),
-    ],
-    (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        // if the error's obj is not empty
-        if(!errors.isEmpty()) {
-            return res.status(400).send(errors.array());
-        }
-
-        const { email, password } = req.body;
-
-        console.log('Creating a user...');
-        res.send({});
-    }
-);
+router.post('/api/users/signin', (req, res) => {
+    res.send('hey is signInRouter working?');
+});
 
 export { router as signInRouter };
--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,9 +1,33 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-    res.send('hey is signUpRouter working?');
-});
+router.post(
+    '/api/users/signup',
+    [
+        body('email').isEmail().withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 30 })
+            .withMessage('Password must be between 4 and 30 characters'),
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        // if the errors' obj is not empty
+        if(!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array());
+        }
+
+        const { email, password } = req.body;
+
+        console.log('Creating a user...');
+        // If database is down
+        throw new DatabaseConnectionError();
+        res.send({});
+    }
+);
 
 export { router as signUpRouter };

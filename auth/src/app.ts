05956f22487bd2c2f4ae0062e90c4fadcfc6002a
Genadi <genadi.kozarev@gmail.com>
2023-09-15 18:14:10 +0100

    separate the start and configuration of the app

---
+++ auth/src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/sign-in';
+import { signOutRouter } from './routes/sign-out';
+import { signUpRouter } from './routes/sign-up';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+/* 
+Make sure express is aware that is behind a proxy of ingress-nginx
+and should still trust traffic as being secure even thou it's coming from that proxy
+*/
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        // Disable encryption because the JWT is already encrypted
+        signed: false,
+        // Cookies will only be used if the user is visiting via https connection
+        secure: true,
+    })
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,58 +1,21 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/sign-in';
-import { signOutRouter } from './routes/sign-out';
-import { signUpRouter } from './routes/sign-up';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-/* 
-Make sure express is aware that is behind a proxy of ingress-nginx
-and should still trust traffic as being secure even thou it's coming from that proxy
-*/
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-    cookieSession({
-        // Disable encryption because the JWT is already encrypted
-        signed: false,
-        // Cookies will only be used if the user is visiting via https connection
-        secure: true,
-    })
-);
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
-  }
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
 
-  try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log('Connected to MongoDB');
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error(err);
+    }
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000');
-  });
-}
+    app.listen(3000, () => {
+        console.log('Listening on port 3000');
+    });
+};
 
 start();

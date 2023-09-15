f5656c368b157be64af386c12ea0d3f838bd4cb7
Genadi <genadi.kozarev@gmail.com>
2023-09-15 11:48:57 +0100

    /signin endpoint check for valid email and password

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,9 +1,27 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+// body: Method used as a middleware to validate incoming data on the body of the POST request
+import { body, validationResult } from 'express-validator';
+
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('hey is signInRouter working?');
-});
+router.post(
+    '/api/users/signin',
+    [
+        body('email').isEmail().withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must supply a password'),
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array());
+        }
+    }
+);
 
 export { router as signInRouter };

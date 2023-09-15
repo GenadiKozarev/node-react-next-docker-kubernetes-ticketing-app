1c4a229734892c8984164ee19050a8325142f50f
Genadi <genadi.kozarev@gmail.com>
2023-09-15 19:07:28 +0100

    add test script and setup file

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,29 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose, { connect } from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+
+beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+
+    await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+    // Get all existing collections
+    const collections = await mongoose.connection.db.collections();
+
+    // Loop over them and delete all data inside
+    for (let collection of collections) {
+        await collection.deleteMany({});
+    }
+});
+
+afterAll(async () => {
+    if (mongo) {
+        await mongo.stop();
+    }
+    await mongoose.connection.close();
+});

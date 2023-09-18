e37f3eb21a6f72e248c619200c2826280f0f1721
Genadi <genadi.kozarev@gmail.com>
2023-09-18 16:42:05 +0100

    frontend - enable account sign up and handle validation errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,12 +1,19 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async e => {
         e.preventDefault();
-        console.log(email, password);
+
+        try {
+            await axios.post('/api/users/signup', { email, password });
+        } catch (err) {
+            setErrors(err.response.data.errors);
+        }
     };
 
     return (
@@ -16,7 +23,7 @@ const SignUp = () => {
                 <label>Email Address</label>
                 <input
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={e => setEmail(e.target.value)}
                     className='form-control'
                 />
             </div>
@@ -24,11 +31,21 @@ const SignUp = () => {
                 <label>Password</label>
                 <input
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={e => setPassword(e.target.value)}
                     type='password'
                     className='form-control'
                 />
             </div>
+            {errors.length > 0 && (
+                <div className='alert alert-danger'>
+                    <h4>Ooops...</h4>
+                    <ul className='my-0'>
+                        {errors.map(err => (
+                            <li key={err.message}>{err.message}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
             <button className='btn btn-primary'>Sign Up</button>
         </form>
     );

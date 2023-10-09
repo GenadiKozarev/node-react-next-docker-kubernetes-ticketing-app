a167577531fed0e40736a83f329d55f994b54e98
Genadi <genadi.kozarev@gmail.com>
2023-10-09 23:06:53 +0100

    start writing tests for the '/api/tickets' endpoint

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@library-of-knowledge/common';
+import { errorHandler, NotFoundError, currentUser } from '@library-of-knowledge/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 /* 
@@ -22,6 +23,9 @@ app.use(
         secure: process.env.NODE_ENV !== 'test',
     })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();

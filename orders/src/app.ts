147794820170c7836ed610b15388edb51087dc0c
Genadi <genadi.kozarev@gmail.com>
2023-11-11 18:28:31 +0000

    Orders Service initial setup

---
+++ orders/src/app.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@library-of-knowledge/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+/* 
+Make sure express is aware that is behind a proxy of ingress-nginx
+and should still trust traffic as being secure even thou it's coming from that proxy
+*/
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        // Disable encryption because the JWT is already encrypted
+        signed: false,
+        /*
+        Cookies will only be used if the user is visiting via https connection
+        and we're not in a test environment
+        */
+        secure: process.env.NODE_ENV !== 'test',
+    })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };

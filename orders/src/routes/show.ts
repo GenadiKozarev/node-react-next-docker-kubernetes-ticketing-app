be43541a024984b34a3e1ebe7d6b6d5c7b255212
Genadi <genadi.kozarev@gmail.com>
2023-11-12 22:40:18 +0000

    update route to get a particular order by its id

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from 'express';
+import {
+    NotAuthorizedError,
+    NotFoundError,
+    requireAuth,
+} from '@library-of-knowledge/common';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-    res.send({});
-});
+router.get(
+    '/api/orders/:orderId',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        // Get the order and its associated ticket
+        const order = await Order.findById(req.params.orderId).populate(
+            'ticket'
+        );
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+        // If the user does NOT own this order
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+
+        res.send(order);
+    }
+);
 
 export { router as showOrderRouter };

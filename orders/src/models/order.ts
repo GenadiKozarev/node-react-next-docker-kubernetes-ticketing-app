2ebfc468beec10f523d4f5da3c8d810abb31d740
Genadi <genadi.kozarev@gmail.com>
2023-11-29 22:04:31 +0000

    add the 'mongoose-update-if-current' module into the Orders model

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@library-of-knowledge/common';
 import { TicketDoc } from './ticket';
 
@@ -51,6 +52,10 @@ const orderSchema = new mongoose.Schema(
         },
     }
 );
+
+// Update the default mongodb '__v' to be 'version'
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
     return new Order(attrs);

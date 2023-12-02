176e18510db01cc784618712d202b73393bbbd76
Genadi <genadi.kozarev@gmail.com>
2023-12-02 14:18:06 +0000

    expiration service: create an OrderCreatedListener and a new expirationQueue to handle jobs wired up to that listener

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -5,10 +5,18 @@ import {
 } from '@library-of-knowledge/common';
 import { queueGroupName } from './queue-group-name';
 import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     readonly subject = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
-    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {};
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        // add a job to the queue
+        await expirationQueue.add({
+            orderId: data.id,
+        });
+
+        msg.ack();
+    }
 }

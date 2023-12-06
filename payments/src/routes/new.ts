031795643dc71a380c2b7f9db2e4ffb27d79d8c6
Genadi <genadi.kozarev@gmail.com>
2023-12-06 23:00:20 +0000

    Payments service: start adding business logic and checks when making a payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,14 @@
-import { requireAuth, validateRequest } from '@library-of-knowledge/common';
+import {
+    BadRequestError,
+    NotAuthorizedError,
+    NotFoundError,
+    OrderStatus,
+    requireAuth,
+    validateRequest,
+} from '@library-of-knowledge/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -10,7 +18,24 @@ router.post(
     [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
     validateRequest,
     async (req: Request, res: Response) => {
+        const { token, orderId } = req.body;
+        // find the order the user is trying to pay for
+        const order = await Order.findById(orderId);
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+        // ensure the order belongs to this user
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+        // ensure the order is not yet cancelled
+        if (order.status === OrderStatus.Cancelled) {
+            throw new BadRequestError('Cannot pay for a cancelled order')
+        }
         res.send({ success: true });
+        // TODO:
+        // ensure the payment amount matches the amount due for the order
     }
 );
 

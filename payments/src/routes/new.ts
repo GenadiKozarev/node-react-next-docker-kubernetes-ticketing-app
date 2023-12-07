df3caf6c057bc31107495b52f184969975827145
Genadi <genadi.kozarev@gmail.com>
2023-12-07 22:53:33 +0000

    Payments service: create a Stripe class and add it to the route for new charges

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -8,6 +8,7 @@ import {
 } from '@library-of-knowledge/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -33,6 +34,15 @@ router.post(
         if (order.status === OrderStatus.Cancelled) {
             throw new BadRequestError('Cannot pay for a cancelled order')
         }
+
+        // https://stripe.com/docs/api/charges/create
+        await stripe.charges.create({
+            currency: 'usd',
+            // stripe uses the smallest currency unit (e.g., 100 cents to charge $1.00 or 100 to charge ¥100, a zero-decimal currency)
+            amount: order.price * 100,
+            source: token,
+        });
+
         res.send({ success: true });
         // TODO:
         // ensure the payment amount matches the amount due for the order
